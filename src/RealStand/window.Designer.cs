ea6f5e7b820a4a69acaf181f1b12a2cbc2757ee7
Micael Rodrigues <micaelr95@outlook.pt>
2019-05-17 19:29:54 +0100

    Added tabcontrol with buttons

---
+++ src/RealStand/window.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace RealStand
+{
+    partial class window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageInicio = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPageClientes = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPageOficina = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPageVendas = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPageAluguer = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPageInicio.SuspendLayout();
+            this.tabPageClientes.SuspendLayout();
+            this.tabPageOficina.SuspendLayout();
+            this.tabPageVendas.SuspendLayout();
+            this.tabPageAluguer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.Buttons;
+            this.tabControl1.Controls.Add(this.tabPageInicio);
+            this.tabControl1.Controls.Add(this.tabPageClientes);
+            this.tabControl1.Controls.Add(this.tabPageOficina);
+            this.tabControl1.Controls.Add(this.tabPageVendas);
+            this.tabControl1.Controls.Add(this.tabPageAluguer);
+            this.tabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabControl1.ItemSize = new System.Drawing.Size(30, 160);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(20);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(801, 450);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabControl1_DrawItem);
+            // 
+            // tabPageInicio
+            // 
+            this.tabPageInicio.BackColor = System.Drawing.Color.Silver;
+            this.tabPageInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tabPageInicio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabPageInicio.Controls.Add(this.label1);
+            this.tabPageInicio.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tabPageInicio.Location = new System.Drawing.Point(166, 4);
+            this.tabPageInicio.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageInicio.Name = "tabPageInicio";
+            this.tabPageInicio.Size = new System.Drawing.Size(631, 442);
+            this.tabPageInicio.TabIndex = 0;
+            this.tabPageInicio.Text = "Início";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Início";
+            // 
+            // tabPageClientes
+            // 
+            this.tabPageClientes.BackColor = System.Drawing.Color.Silver;
+            this.tabPageClientes.Controls.Add(this.label2);
+            this.tabPageClientes.Location = new System.Drawing.Point(166, 4);
+            this.tabPageClientes.Name = "tabPageClientes";
+            this.tabPageClientes.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageClientes.Size = new System.Drawing.Size(631, 442);
+            this.tabPageClientes.TabIndex = 1;
+            this.tabPageClientes.Text = "Clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Clientes";
+            // 
+            // tabPageOficina
+            // 
+            this.tabPageOficina.BackColor = System.Drawing.Color.Silver;
+            this.tabPageOficina.Controls.Add(this.label3);
+            this.tabPageOficina.Location = new System.Drawing.Point(166, 4);
+            this.tabPageOficina.Name = "tabPageOficina";
+            this.tabPageOficina.Size = new System.Drawing.Size(631, 442);
+            this.tabPageOficina.TabIndex = 2;
+            this.tabPageOficina.Text = "Oficina";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Oficina";
+            // 
+            // tabPageVendas
+            // 
+            this.tabPageVendas.BackColor = System.Drawing.Color.Silver;
+            this.tabPageVendas.Controls.Add(this.label4);
+            this.tabPageVendas.Location = new System.Drawing.Point(166, 4);
+            this.tabPageVendas.Name = "tabPageVendas";
+            this.tabPageVendas.Size = new System.Drawing.Size(631, 442);
+            this.tabPageVendas.TabIndex = 3;
+            this.tabPageVendas.Text = "Vendas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Vendas";
+            // 
+            // tabPageAluguer
+            // 
+            this.tabPageAluguer.BackColor = System.Drawing.Color.Silver;
+            this.tabPageAluguer.Controls.Add(this.label5);
+            this.tabPageAluguer.Location = new System.Drawing.Point(166, 4);
+            this.tabPageAluguer.Name = "tabPageAluguer";
+            this.tabPageAluguer.Size = new System.Drawing.Size(631, 442);
+            this.tabPageAluguer.TabIndex = 4;
+            this.tabPageAluguer.Text = "Aluguer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Aluguer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageInicio.ResumeLayout(false);
+            this.tabPageInicio.PerformLayout();
+            this.tabPageClientes.ResumeLayout(false);
+            this.tabPageClientes.PerformLayout();
+            this.tabPageOficina.ResumeLayout(false);
+            this.tabPageOficina.PerformLayout();
+            this.tabPageVendas.ResumeLayout(false);
+            this.tabPageVendas.PerformLayout();
+            this.tabPageAluguer.ResumeLayout(false);
+            this.tabPageAluguer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageInicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPageClientes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPageOficina;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPageVendas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPageAluguer;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
