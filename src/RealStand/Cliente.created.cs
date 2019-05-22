682e0d0d13f12ff0aa73ddf669982147f6edef5f
MartimTSilva <43602260+MartimTSilva@users.noreply.github.com>
2019-05-22 14:25:53 +0100

    Cliente Class

---
+++ src/RealStand/Cliente.created.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStand
+{
+    public partial class Cliente
+    {
+        public Cliente(string nome, int nIF, string morada, string contacto)
+        {
+            Nome = nome;
+            NIF = nIF;
+            Morada = morada;
+            Contacto = contacto;
+            CarroOficina = new HashSet<CarroOficina>();
+        }
+
+        public override string ToString()
+        {
+            return Nome + " (" + NIF + ")";
+        }
+    }
+}
